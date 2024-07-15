2ea5b6928b4db6ddff73c20711495ef148e5d567
vicziani <viczian.istvan@gmail.com>
2024-07-15 21:51:31 +0200

    New test case: find by id

--- src/test/java/employees/EmployeesApplicationIT.java
+++ src/test/java/employees/EmployeesApplicationIT.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EmployeesApplicationIT {
 
@@ -24,6 +26,29 @@ class EmployeesApplicationIT {
 				.exchange()
 				.expectStatus()
 					.isCreated();
+	}
+
+	@Test
+	void findById() {
+		var created = webTestClient
+				.post()
+				.uri("/api/employees")
+				.bodyValue(new EmployeeResource("John Doe"))
+				.exchange()
+				.expectStatus()
+				.isCreated()
+				.returnResult(EmployeeResource.class)
+				.getResponseBody()
+				.blockFirst();
+
+		webTestClient
+				.get()
+				.uri("/api/employees/{id}", created.id())
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(EmployeeResource.class)
+				.value(employee -> assertEquals("John Doe", employee.name()));
+
 	}
 
 }
