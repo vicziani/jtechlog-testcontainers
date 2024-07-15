9a7bd5e9818e71c1d92021ef031d3c2ef96a4006
vicziani <viczian.istvan@gmail.com>
2024-07-15 23:07:46 +0200

    Move test cases to new test class

--- src/test/java/employees/EmployeesApplicationIT.java
+++ src/test/java/employees/EmployeesApplicationIT.java
@@ -7,48 +7,11 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class EmployeesApplicationIT {
-
-	@Autowired
-	WebTestClient webTestClient;
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void save() {
-		webTestClient
-				.post()
-				.uri("/api/employees")
-				.bodyValue(new EmployeeResource("John Doe"))
-				.exchange()
-				.expectStatus()
-					.isCreated();
-	}
-
-	@Test
-	void findById() {
-		var created = webTestClient
-				.post()
-				.uri("/api/employees")
-				.bodyValue(new EmployeeResource("John Doe"))
-				.exchange()
-				.expectStatus()
-				.isCreated()
-				.returnResult(EmployeeResource.class)
-				.getResponseBody()
-				.blockFirst();
-
-		webTestClient
-				.get()
-				.uri("/api/employees/{id}", created.id())
-				.exchange()
-				.expectStatus().isOk()
-				.expectBody(EmployeeResource.class)
-				.value(employee -> assertEquals("John Doe", employee.name()));
-
 	}
 
 }
